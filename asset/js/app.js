d09a3ed95aa66184e3ca1d3ba11848739d29f5c5
TCHANA-NGASSAM <ngassamadriano@gmail.com>
2023-10-04 18:06:29 +0200

    ajout des autre fonctionnlites

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,16 +1,40 @@
-
-
+let afficherOperation = document.getElementById("afficherOperation")
 let afficherResultat = document.getElementById("afficherResultat")
 
 function ajouterChiffre(chiffre) {
-  afficherResultat.textContent += chiffre
+  afficherOperation.value += chiffre
+  afficherResultat.value = ''
 }
 
  function ajouterOperation(operation) {
-  afficherResultat.textContent  += operation;
+  afficherOperation.value  += operation;
+  afficherResultat.value = ''
 }
 
  function effacerTout() {
-    afficherResultat.textContent= ""
+    afficherOperation.value= ""
+    afficherResultat.value = ""
  }
+
+ function effacerUnElement(){
+  let affichage = afficherOperation.value
+  let nouvelleAffichage = affichage.slice(0,-1)
+  afficherOperation.value = nouvelleAffichage
+ }
+function calculer() {
+  if (afficherOperation) {
+   try {
+    let resultat = eval(afficherOperation.value);
+    afficherResultat.value  = resultat;
+   } catch (e){
+    afficherResultat.value  = "Erreur";
+    console.log(e)
+
+   }
+  } else{
+    afficherResultat.value  = "Erreur";
+    console.log(afficherOperation)
+  }
+  afficherOperation.value = ''
+}
 

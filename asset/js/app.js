4cab7e3c8fa3dd6be09a4b90b208149600f20f56
TCHANA-NGASSAM <ngassamadriano@gmail.com>
2023-10-04 12:48:56 +0200

    ajout du box shadow

--- asset/js/app.js
+++ asset/js/app.js
@@ -11,26 +11,6 @@ function ajouterChiffre(chiffre) {
 }
 
  function effacerTout() {
-  
-  if (afficherResultat !== ''){
     afficherResultat.textContent= ""
-  }else{
-    afficherResultat.textContent= 'ADRIEN CALCULATRICE'
-  }
  }
-
- function effacerUnElement(){
-  let affichage = afficherResultat.textContent
-  let nouvelleAffichage = affichage.slice(0,-1)
-  afficherResultat.textContent = nouvelleAffichage
- }
-
-function calculer() {
-  if (afficherResultat) {
-    let resultat = eval(afficherResultat.textContent);
-     afficherResultat.textContent  = resultat;
-  } else{
-     afficherResultat.textContent  = "Erreur";
-  }
-}
 
